ba9d3e32fa9aa45ff9f128636c09abebec514196
Mengli Xia <morryxia@gmail.com>
2015-10-01 22:11:44 +0800

    修改touchmove事件的组织

--- static/js/main.js
+++ static/js/main.js
@@ -219,9 +219,9 @@ var draw = function(){
 //   } 
  
 // }
-$('body').bind('touchmove',function(event){
-  event.preventDefault();
-});
+// $('body').bind('touchmove',function(event){
+//   event.preventDefault();
+// });
 init();
 
 var jQline = $("#j-line"), jQnum = $("#j-num")
