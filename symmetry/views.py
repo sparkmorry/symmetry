b317c9c4406fa77b4101d2ab06e6053be129c570
Mengli Xia <morryxia@gmail.com>
2015-10-01 17:46:45 +0800

    symmetry-django

---
+++ symmetry/views.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+import json
+
+def home(request):
+	js='main'
+	return render_to_response('index.html', {'js': js})
